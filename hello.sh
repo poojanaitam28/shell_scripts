1b313b3f78f096457a7fb375e6f7399fb6e68127
poojanaitam28 <122607216+poojanaitam28@users.noreply.github.com>
2024-10-25 15:53:02 +0530

    Create hello.sh

---
+++ hello.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Single line comment
+
+<<comment
+multi-line comment
+comment
+
+echo "Gabbar: Kitne Aadmi the??"
+
+echo "Kalia: 2 Sarkar."
+
+echo ""
+
+echo "Gabbar: kitne baje hai"
+
+date
+
+echo ""
+
+echo "Gabbar: kab se class chl rhi hai"
+
+uptime
